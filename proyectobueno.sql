acacec4e1b612e1d4ce8ee3b2ed13f53606f7fc6
David Jurado <hdjp1998@gmail.com>
2019-03-04 14:20:49 +0100

    Actualizacion de base de datos

--- proyectobueno.sql
+++ proyectobueno.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: proyecto
 -- ------------------------------------------------------
@@ -25,12 +25,12 @@ DROP TABLE IF EXISTS `citas`;
 CREATE TABLE `citas` (
   `cod_cita` int(11) NOT NULL AUTO_INCREMENT,
   `fecha` date NOT NULL,
-  `dni` varchar(9) NOT NULL,
   `cod_clientes` int(11) NOT NULL,
+  `hora` time NOT NULL,
   PRIMARY KEY (`cod_cita`),
   KEY `fk_citas_1_idx` (`cod_clientes`),
   CONSTRAINT `fk_citas_1` FOREIGN KEY (`cod_clientes`) REFERENCES `clientes` (`cod_clientes`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `citas` (
 
 LOCK TABLES `citas` WRITE;
 /*!40000 ALTER TABLE `citas` DISABLE KEYS */;
+INSERT INTO `citas` VALUES (1,'2012-12-12',1,'00:00:00'),(2,'2010-10-10',1,'09:30:00'),(3,'2012-12-12',1,'00:00:00');
 /*!40000 ALTER TABLE `citas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,14 +54,13 @@ CREATE TABLE `clientes` (
   `cod_clientes` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `dni` char(9) NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `apellidos` varchar(45) NOT NULL,
-  `domicilio` varchar(45) NOT NULL,
   `telefono` varchar(9) NOT NULL,
+  `domicilio` varchar(45) NOT NULL,
   `sexo` varchar(45) NOT NULL,
   PRIMARY KEY (`cod_clientes`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `clientes` (
 
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
-INSERT INTO `clientes` VALUES (1,'admin@gmail.com','21232f297a57a5a743894a0e4a801fc3','admin','admin','admin','admin','admin','admin'),(2,'a@a','0cc175b9c0f1b6a831c399e269772661','a','a','a','a','a','a'),(3,'b@b','92eb5ffee6ae2fec3ad71c777531578f','b','b','b','b','b','Masculino');
+INSERT INTO `clientes` VALUES (1,'admin@gmail.com','21232f297a57a5a743894a0e4a801fc3','admin','admin','admin','admin','Masculino'),(2,'a@a','0cc175b9c0f1b6a831c399e269772661','a','a','691579104','Pepa','Femenino'),(3,'lkadm@af','7815696ecbf1c96e6894b779456d330e','asd','das','999999999','asdasd','Masculino'),(4,'alejandro_roman_caballero@outlook.es','7ed1ca45414f40612d0c469e24453e40','Alejandro','Roman','677365255','Sevilla, España','Masculino');
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,13 +81,13 @@ DROP TABLE IF EXISTS `peluquero`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `peluquero` (
-  `cod_peluquero` int(11) NOT NULL,
+  `cod_peluquero` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
   `apellidos` varchar(45) NOT NULL,
   `fecha_contrato` date NOT NULL,
   `sexo` varchar(45) NOT NULL,
   PRIMARY KEY (`cod_peluquero`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,6 +96,7 @@ CREATE TABLE `peluquero` (
 
 LOCK TABLES `peluquero` WRITE;
 /*!40000 ALTER TABLE `peluquero` DISABLE KEYS */;
+INSERT INTO `peluquero` VALUES (1,'David','Izquierdo','2018-06-22','Masculino'),(7,'fdsa','sddas','2019-02-01','Femenino');
 /*!40000 ALTER TABLE `peluquero` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,10 +109,10 @@ DROP TABLE IF EXISTS `servicio`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `servicio` (
   `cod_servicio` int(11) NOT NULL AUTO_INCREMENT,
-  `servicio` varchar(45) NOT NULL,
+  `servicios` varchar(45) NOT NULL,
   `precio` int(11) NOT NULL,
   PRIMARY KEY (`cod_servicio`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,7 +121,7 @@ CREATE TABLE `servicio` (
 
 LOCK TABLES `servicio` WRITE;
 /*!40000 ALTER TABLE `servicio` DISABLE KEYS */;
-INSERT INTO `servicio` VALUES (1,'                   Corte de cabello',7),(2,'           Arreglo de barbita',3),(3,'Corte cabello niño (hasta 10 años)',6),(4,'Arreglo cuello y patillas',2),(5,'Perfilar Barba',2);
+INSERT INTO `servicio` VALUES (1,' Corte de cabello',7),(2,'Arreglo de barba',3),(3,' Corte de cabello niño (hasta 10 años)',6),(4,'Arreglo cuello y patillas',2),(5,'Perfilar barba',2),(6,'Lavado extra',1),(7,'  Servicio a domicilio',10);
 /*!40000 ALTER TABLE `servicio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,7 +144,7 @@ CREATE TABLE `servicio_prestado` (
   CONSTRAINT `fk_servicio_prestado_1` FOREIGN KEY (`cod_peluquero`) REFERENCES `peluquero` (`cod_peluquero`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_servicio_prestado_2` FOREIGN KEY (`cod_servicio`) REFERENCES `servicio` (`cod_servicio`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_servicio_prestado_3` FOREIGN KEY (`cod_cita`) REFERENCES `citas` (`cod_cita`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -152,6 +153,7 @@ CREATE TABLE `servicio_prestado` (
 
 LOCK TABLES `servicio_prestado` WRITE;
 /*!40000 ALTER TABLE `servicio_prestado` DISABLE KEYS */;
+INSERT INTO `servicio_prestado` VALUES (1,1,1,1),(7,3,2,1);
 /*!40000 ALTER TABLE `servicio_prestado` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -164,4 +166,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-24  0:28:42
+-- Dump completed on 2019-03-04 14:12:55
